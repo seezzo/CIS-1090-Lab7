d56ea465cf5c4737e85c17db88e5fbd54adfd200
Test Student <cuteststudent@billkuker.com>
2022-10-19 08:58:16 -0400

    Binary search questions

--- dictionary-binary-search.js
+++ dictionary-binary-search.js
@@ -1,21 +1,7 @@
 import words from "./words.js";
 
 function binarySearch(needle, haystack){
-    let start = 0;
-    let end = haystack.length;
-    while ( start < end ){
-        let pivot = Math.floor((start + end) / 2);
-        let wordAtPivot = haystack[pivot];
-        //console.log(start, end, pivot, wordAtPivot);
-
-        if ( wordAtPivot < needle ){
-            start = pivot+1;
-        } else if ( wordAtPivot > needle ){
-            end = pivot;
-        } else {
-            return true;
-        }
-    }
+    //❓ Question 7, write it!
     return false;
 }
 
